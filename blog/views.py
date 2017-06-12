cedfdd1883f836a00eb0f91ef51e86cb782a2957
jaco <fbwotjq@gmail.com>
2017-06-13 00:37:32 +0900

    간단한 블로그 화면 뷰 관련 코딩

--- blog/views.py
+++ blog/views.py
@@ -31,10 +31,12 @@ class PostYAV(YearArchiveView) :
 class PostMAV(MonthArchiveView) :
     model = Post
     date_field = 'modify_date'
+    month_format = "%m"
 
 class PostDAV(DayArchiveView) :
     model = Post
     date_field = 'modify_date'
+    month_format = "%m"
 
 class PostTAV(TodayArchiveView) :
     model = Post
